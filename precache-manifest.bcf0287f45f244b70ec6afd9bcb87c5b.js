3b0845039b66427cb4f2fd90276aa9f1c68be122
hrushkooleksii <hrushkooleksii@gmail.com>
2020-06-19 17:56:51 +0300

    Updates

---
+++ precache-manifest.bcf0287f45f244b70ec6afd9bcb87c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67caecfd52a2b46c633a859e0f1f2bc1",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9e1b56007dcf6e865f73",
+    "url": "/goit-react-hw-02-feedback/static/css/main.95d03154.chunk.css"
+  },
+  {
+    "revision": "ede8bda84952a70c5fae",
+    "url": "/goit-react-hw-02-feedback/static/js/2.98f13a9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.98f13a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1b56007dcf6e865f73",
+    "url": "/goit-react-hw-02-feedback/static/js/main.32c20f9e.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
